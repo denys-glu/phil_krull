7dd5cdee68ea6d472faeefba46f85b840b8e376b
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-12 01:11:15 -0500

    adding day 2 challenges

---
+++ algorithms/weekOne/dayTwo.js
@@ -0,0 +1,9 @@
+// Today's Algo challenges (Do not use any built in JS methods):
+//  Array: Push Front
+    function pushFront(arr, val) {}
+//  Array: Pop Front, remove and return first val
+    function popFront(arr) {}
+//  Array: Insert At, insert at the index
+    function insertAt(arr, index, val) // ([], 0, 1)
+//  Array: Remove At
+    function removeAt(arr, index)
