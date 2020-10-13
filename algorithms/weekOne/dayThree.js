5ffc6dd55b73fff44c35bfb44361305f013e6b88
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-13 00:59:10 -0500

    adding day three algo challenges

---
+++ algorithms/weekOne/dayThree.js
@@ -0,0 +1,20 @@
+// Reverse Array
+// -----------------------------------------------------
+// Given a numerical array, reverse the order of the
+// values. The reversed array should have the same
+// length, with existing elements moved to other
+// indices so that the order of elements is reversed.
+
+// Remove Negatives
+// -----------------------------------------------------
+// Implement a function removeNegatives() that
+// accepts an array and removes any values that
+// are less than zero.
+// Second-level challenge: don’t use nested loops.
+
+// Skyline Heights
+// -----------------------------------------------------
+// You are given an array with heights of consecutive buildings in the city. For example, [-1,7,3] would
+// represent three buildings: first is actually below street level, second is seven stories high, and third is
+// three stories high (but hidden behind the seven-story onbe). You are situated at street level. Return an
+// array containing heights of the buildings you can see, in order. Given [1,-1,7,3] return [1,7].
