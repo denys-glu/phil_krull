89819d8f0b6836a7b3b226508093930c9d4db41e
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-14 00:55:51 -0500

    adding day four challenges

---
+++ algorithms/weekOne/dayFour.js
@@ -0,0 +1,24 @@
+// Rotate Array
+// -----------------------------------------------------
+// Implement rotateArr(arr, shiftBy) that
+// accepts array and offset. Shift arr’s values to the
+// right by that amount. ‘Wrap-around’ any values
+// that shift off array’s end to the other side, so that
+// no data is lost. Operate in-place: given
+// ([1,2,3],1), change the array to [3,1,2].
+
+// Optionally, add these advanced features:
+//     a) allow a negative shiftBy (shift left, not right),
+//     b) minimize memory usage. With only a few local
+//         variables (not an array), handle arrays and
+//         shiftBys in the millions,
+//     c) minimize how many touches of each element.
+
+
+
+// Min Of Sorted-Rotated
+// -----------------------------------------------------
+// You will be given a numerical array that has first
+// been sorted, then rotated by an unknown
+// amount. Find and return the minimum value in
+// that array.
