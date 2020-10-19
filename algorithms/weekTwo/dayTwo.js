ecefc71d10004884aad54617e907e5cfb1b50ad2
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-19 01:17:24 -0500

    updating week 2 day 2 challenges

--- algorithms/weekTwo/dayTwo.js
+++ algorithms/weekTwo/dayTwo.js
@@ -6,3 +6,12 @@
 // -------------------------------------------------------
 // Given an array, return the Nth-largest element:
 // there should be (N - 1) elements that are larger.
+
+function nthLargest(arr, n) {}
+
+
+// Shuffle
+// -------------------------------------------------------
+// Recreate the shuffle()built into JavaScript, to
+// efficiently shuffle a given array’s values. Do you
+// need to return anything from your function?
