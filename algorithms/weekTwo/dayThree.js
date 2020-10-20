7b69183c8fc6ae7a7a7e39334f8b9c256f823aa0
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-20 01:02:09 -0500

    adding week 2 day 2 challenges

---
+++ algorithms/weekTwo/dayThree.js
@@ -0,0 +1,11 @@
+// Binary Search
+// ------------------------------------------------------------
+// Given a sorted array and a value, return whether
+// that value is present in the array. Do not
+// sequentially iterate the entire array. Instead,
+// ‘divide and conquer’, taking advantage of the fact
+// that the array is sorted.
+
+// Pairs to Sum
+// ------------------------------------------------------------
+// How do you find all pairs of an integer array whose sum is equal to a given number?
