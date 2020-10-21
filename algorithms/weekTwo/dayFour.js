8cff74f69fd3de8dc5250acd4d4aecd44bf5ae1f
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-21 01:05:54 -0500

    adding week 2 day 4 challenges

---
+++ algorithms/weekTwo/dayFour.js
@@ -0,0 +1,16 @@
+// Find maximum product of two integers in an array
+// ---------------------------------------------------------------------------------
+// Given an array of integers, find maximum product of two intefegs in an array
+// input = [4,7,2,9,5]
+// output = 63 (7*9)
+// input =[5,-4,2,6,-8]
+// output = 32 (-8*-4)
+
+
+
+// Move all zeros present in an array to the end
+// ---------------------------------------------------------------------------------
+// Given an array of intefers, move all zeros present in the array to the end.  The solution should maintain the relative order of items in the array.
+
+// input = [6,0,8,2,3,4,0,4,0,1]
+// output = [6,8,2,3,4,4,1,0,0,0]
--- algorithms/weekTwo/dayThree.js
+++ algorithms/weekTwo/dayThree.js
@@ -5,7 +5,18 @@
 // sequentially iterate the entire array. Instead,
 // ‘divide and conquer’, taking advantage of the fact
 // that the array is sorted.
+[2,40,66,99,300]
 
 // Pairs to Sum
 // ------------------------------------------------------------
 // How do you find all pairs of an integer array whose sum is equal to a given number?
+function pairsToSum(arr, num){}
+
+pairsToSum([3,8,5,1,8,7,3,4,0], 8)
+[
+    [8,0],
+    [1,7],
+    [8,0],
+    [3,5],
+    [5,3]
+]
