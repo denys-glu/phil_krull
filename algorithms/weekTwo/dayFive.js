c5fc64c14a9c3292162866ac36ca8abbdcd129b2
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-22 00:57:17 -0500

    adding week 2 day 5 challenges

---
+++ algorithms/weekTwo/dayFive.js
@@ -0,0 +1,17 @@
+// MostFrequentNumber
+// ------------------------------------------------------------------------
+// Given an array of interfers containing duplicates,return the majority element in the array if Present.
+// Input = [2,8,7,2,2,5,2,3,1,2,2]
+// Output = 2
+
+// Number of Steps to Reduce a Number to Zero
+// // ------------------------------------------------------------------------
+// Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+// Example
+// Input: num = 8
+// Output: 4
+// Explanation: 
+// Step 1) 8 is even; divide by 2 and obtain 4. 
+// Step 2) 4 is even; divide by 2 and obtain 2. 
+// Step 3) 2 is even; divide by 2 and obtain 1. 
+// Step 4) 1 is odd; subtract 1 and obtain 0.
