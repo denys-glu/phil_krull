d2c821a7585d6eab9e39d22138d63a8ecbc56eb0
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-25 00:56:12 -0500

    adding first day of python challenges

---
+++ algorithms/weekThree/dayOne.js
@@ -0,0 +1,17 @@
+// Arrs2Map
+// ----------------------------------------------------------------------------------------------------------------
+// Given two arrays, create an associative array (map) containing keys of the first, and values of the
+// second. For arr1 = ["abc", 3, "yo"] and arr2 = [42, "wassup", true], return {"abc":
+// 42, 3: "wassup", "yo": true}.
+
+// InvertHash
+// ----------------------------------------------------------------------------------------------------------------
+// Create invertHash(assocArr) that converts a hash’s keys to values and values to corresponding keys.
+// Example: given {"name": "Zaphod"; "numHeads": 2}, return {"Zaphod": "name"; 2:
+// "numHeads"}. You will need to learn and use a JavaScript for ... in h ere!
+
+// ReverseString
+// ----------------------------------------------------------------------------------------------------------------
+// Implement a function reverseString(str) that, given a string, will return the string of the same length but
+// with characters reversed. Example: given "creature", return "erutaerc". Do not use the built-in
+// reverse() function!
