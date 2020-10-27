7c54d3a286d223f3eb84a8fd23e0ce4dae17f63f
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-27 01:11:00 -0500

    updated w3d3 challenge

--- algorithms/weekThree/dayThree.py
+++ algorithms/weekThree/dayThree.py
@@ -1,7 +1,7 @@
-Parens Valid
-----------------------------------------------------------------------------------------
-Create a function that, given an input string, returns a boolean whether parentheses in that string are valid. Given input "y(3(p)p(3)r)s", return true. Given "n(0(p)3", return false. Given "n)0(t(0)k", return false.
+# Parens Valid
+# ----------------------------------------------------------------------------------------
+# Create a function that, given an input string, returns a boolean whether parentheses in that string are valid. Given input "y(3(p)p(3)r)s", return true. Given "n(0(p)3", return false. Given "n)0(t(0)k", return false.
 
-Braces Valid
-----------------------------------------------------------------------------------------
-Given a string, returns whether the sequence of various parentheses, braces and brackets within it are valid. For example, given the input string "w(a{t}s[o(n{c}o)m]e)h[e{r}e]!", return true. Given "d(i{a}l[t]o)n{e", return false. Given "a(1)s[O(n]0{t)0}k", return false.
+# Braces Valid
+# ----------------------------------------------------------------------------------------
+# Given a string, returns whether the sequence of various parentheses, braces and brackets within it are valid. For example, given the input string "w(a{t}s[o(n{c}o)m]e)h[e{r}e]!", return true. Given "d(i{a}l[t]o)n{e", return false. Given "a(1)s[O(n]0{t)0}k", return false.
