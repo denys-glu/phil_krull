95fa210b4d50a6819ef287e3176af0f3129b4777
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-28 04:44:30 -0500

    adding intro to flask lecture

---
+++ lectures/Python/flask_intro/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    print("Index method was triggered")
+    return "In index method"
+
+@app.route("/name/<string:user_name>/<int:number>")
+def say_name(user_name, number):
+    print(f"In say_name method, hi {user_name}")
+    return render_template("index.html", name_for_template = user_name, num_for_template = number)
+
+app.run(debug=True)
+
