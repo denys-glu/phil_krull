be8d25392d593d5c9803e0837a52eb24e1468844
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-28 00:57:20 -0500

    adding w3d4 algo challenges

---
+++ algorithms/weekThree/dayFour.py
@@ -0,0 +1,7 @@
+# Is Palindrome
+# --------------------------------------------------------------------------------
+# Strings like "Able was I, ere I saw Elba" or "Madam, I'm Adam" could be considered palindromes, because (if we ignore spaces, punctuation and capitalization) the letters are the same from front and back. Create a function that returns a boolean whether the string is a strict palindrome. For "a x a" or "racecar", return true. Do not ignore spaces, punctuation and capitalization: if given "Dud" or "oho!", return false.
+
+# Longest Palindrome
+# --------------------------------------------------------------------------------
+# For this challenge, we will look not only at the entire string, but also substrings within it. For a string, return the longest palindromic substring. Given "what up, dada?", return "dad". Given "not much", return "n". Include spaces as well (i.e. be strict, as in the “Is Palindrome” challenge): given "My favorite racecar erupted!", return "e racecar e".
---
+++ oopHackathon.py
@@ -0,0 +1,35 @@
+'''
+Option 1
+Game
+    - levels of difficulty
+    - characters
+
+Classes needed
+    - game
+    - character
+        - good/bad
+
+Min requirements
+    - 5 instances of character
+    - scoring system
+'''
+
+'''
+Option 2
+Users(in charge of other users) <-> Users
+Perform some task
+example
+    - parent -> babysitter
+    - teacher -> students
+    - doctor -> patients
+'''
+
+'''
+Option 3
+3 different classes that interact
+    - 5 instances of interaction
+Scheduler
+    Movies -> rooms -> people
+Sales
+    Customer, seller, product
+'''
