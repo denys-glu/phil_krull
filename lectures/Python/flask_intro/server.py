c8fdc62a3f2c0e111758a0428a42769baa13177d
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-10-29 00:52:15 -0500

    adding w3d5 algo challenges

--- lectures/Python/flask_intro/server.py
+++ lectures/Python/flask_intro/server.py
@@ -7,7 +7,7 @@ def index():
     print("Index method was triggered")
     return "In index method"
 
-@app.route("/name/<string:user_name>/<int:number>")
+@app.route("/name", defaults={"user_name": "phil", "number": 0})
 def say_name(user_name, number):
     print(f"In say_name method, hi {user_name}")
     return render_template("index.html", name_for_template = user_name, num_for_template = number)
