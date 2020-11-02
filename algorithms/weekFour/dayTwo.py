6f741e2dc00d7a49f12cb2ec6ff94d02d1326578
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-01 23:59:24 -0600

    adding w4d2 algo challenges

---
+++ algorithms/weekFour/dayTwo.py
@@ -0,0 +1,15 @@
+# length
+# --------------------------------------------------------------------------------------------------
+# Create a function that accepts a pointer to first list node, and returns number of nodes in sList.
+
+# average
+# --------------------------------------------------------------------------------------------------
+# Create a standalone function average(node) that returns (…wait for it … ) the average of all values contained in that list.
+
+# min, max
+# --------------------------------------------------------------------------------------------------
+# Create function min(node) and max(node) to returning smallest and largest values in the list.
+
+# display
+# --------------------------------------------------------------------------------------------------
+# Create display(node) for debugging that returns a string containing all list values. Build what you wish console.log(myList) did!
