0ce355deb7fe29bf0d7a8237d5d1d8eb4c5a0f48
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-04 06:29:21 -0600

    adding Django ManyToMany lecture

--- lectures/Python/django/django_orm/first_app/models.py
+++ lectures/Python/django/django_orm/first_app/models.py
@@ -4,12 +4,14 @@ from django.db import models
 
 class Dojo(models.Model):
     name = models.CharField(max_length=55)
+    description = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # hidden key called Students, a list of students
+    # hidden key called visited_by, a list of students
 
     def __repr__(self):
-        return f"<Dojo object: Name = {self.name}, number of students: {self.Students.count()}>"
+        return f"<Dojo object: Name = {self.name}, number of students: {self.Students.count()}, number of visits: {self.visited_by.count()}>"
 
 class Student(models.Model):
     # id -> Django generates this for us
@@ -18,6 +20,13 @@ class Student(models.Model):
     dojo = models.ForeignKey(Dojo, related_name="Students", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    visited = models.ManyToManyField(Dojo, related_name = "visited_by")
 
     def __repr__(self):
-        return f"<Student object: Name = {self.name}, Email = {self.email} dojo = {self.dojo.name}>"
+        return f"<Student object: Name = {self.name}, Email = {self.email} dojo = {self.dojo.name}, dojos visited = {self.visited.count()}>"
+
+
+# class Like(models.Model):
+#     student = models.ForeignKey(Student, related_name="likes", on_delete=models.CASADE)
+#     dojo = models.ForeignKey(Dojo, related_name="liked", on_delete=models.CASADE)
+#     #other other columns
--- lectures/Python/django/django_orm/first_app/views.py
+++ lectures/Python/django/django_orm/first_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Student, Dojo
 
 # Create your views here.
+def index(request):
+    context = {
+        'all_dojos': Dojo.objects.all(),
+        'all_students': Student.objects.all()
+    }
+    return render(request, 'index.html', context)
