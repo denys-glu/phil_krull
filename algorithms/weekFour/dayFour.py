991dbf0c54375dcf324aeb7867832004640ce203
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-04 04:24:50 -0600

    adding ninja gold demo

--- algorithms/weekFour/dayFour.py
+++ algorithms/weekFour/dayFour.py
@@ -2,6 +2,10 @@
 # ------------------------------------------------------------------------------------
 # Given a pointer to the head node of a singly linked list, remove any nodes containing negative values and return the list.
 
+
 # Partition
 # ------------------------------------------------------------------------------------
-# Create partition(ListNode, value) that locates the first node with that value, and moves all nodes with values less than that value to be earlier, and all nodes with values greater than that value to be later.  Otherwise, original order need not ve perfectly preserved. return the new head ListNode.
+# Create partition(ListNode, value) that locates the first node with that value, and moves all nodes with values less than that value to be earlier, and all nodes with values greater or equal to that value to be later.  Otherwise, original order need not be perfectly preserved. return the new head ListNode.
+
+3-6-2-8-4-9-1-6, 4
+1-3-2-4-6-8-9-6
