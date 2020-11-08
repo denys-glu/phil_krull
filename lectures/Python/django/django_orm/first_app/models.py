696cac6a31dcb46ce6cc328e074127cf50ddce03
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-11-08 02:36:29 -0600

    adding Model Manager lecture

--- lectures/Python/django/django_orm/first_app/models.py
+++ lectures/Python/django/django_orm/first_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class DojoManager(models.Manager):
+    def validate(self, post_data):
+        errors = []
+        if len(post_data['name']) < 1:
+            print('Name is required, in dojo manager')
+            errors.append('Name is required!')
+
+        return errors
+
 class Dojo(models.Model):
     name = models.CharField(max_length=55)
     description = models.TextField(default="")
@@ -9,6 +18,11 @@ class Dojo(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # hidden key called Students, a list of students
     # hidden key called visited_by, a list of students
+
+    objects = DojoManager()
+
+    def test(self):
+        print('it works')
 
     def __repr__(self):
         return f"<Dojo object: Name = {self.name}, number of students: {self.Students.count()}, number of visits: {self.visited_by.count()}>"
